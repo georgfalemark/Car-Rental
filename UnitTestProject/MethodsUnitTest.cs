7bc99084cab7d9f654504159f6821de0ac613a37
georgfalemark <georg@falemark.se>
2019-03-12 08:22:04 +0100

    -

--- UnitTestProject/MethodsUnitTest.cs
+++ UnitTestProject/MethodsUnitTest.cs
@@ -24,23 +24,42 @@ namespace UnitTestProject
         }
 
         [TestMethod]
-        public void GetSelectListItems_Customer_1()
+        public async void TryAddBooking_1()
         {
-            Car car = _context.Cars.First();
-            Customer dude = _context.Customers.First();
+            Car car = _context.Cars.Single(x => x.LicenseNumber == "ABC123");
+            Customer dude = _context.Customers.Single(x => x.FirstName == "Georg" && x.LastName == "Fälemark");
+
+            Booking input = new Booking { StartDate = new DateTime(2019, 01, 04), CarId = car.CarId, CustomerId = dude.CustomerId };
+            await _bookingsController.Create(input);
+
+
+
+
+            //Task<IActionResult> expected;
+
+            //hur ska man göra här för att få ett expected? 
+            //Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void TryAddBooking_2()
+        {
+            Car car = null;
+            Customer dude = null;
 
             Booking input = new Booking { StartDate = new DateTime(2019, 01, 04), CarId = car.CarId, CustomerId = dude.CustomerId };
             var result = _bookingsController.Create(input);
 
-            Task<IActionResult> expected;
 
+
+
+            //Task<IActionResult> expected;
 
             //hur ska man göra här för att få ett expected? 
-
-
-
             //Assert.AreEqual(expected, result);
         }
+
+
 
 
 
